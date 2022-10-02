1e0540c9a3f6df6de58457ee0c3a4c199f2c6985
Стас Гайсин <stas.gaisinn@gmail.com>
2022-10-02 23:27:53 +0300

    Added template of myPosts and Posts list requests

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace frontend\controllers;
 
-use yii\rest\ActiveController;
+use Yii;
+use yii\web\Controller;
+use yii\web\MethodNotAllowedHttpException;
 use yii\web\Response;
+use yii\web\ServerErrorHttpException;
 
-class PostController extends ActiveController
+class PostController extends Controller
 {
-    public $modelClass = 'backend\models\Post';
+    public $enableCsrfValidation = false;
 
     public function behaviors(): array
     {
@@ -23,11 +26,61 @@ class PostController extends ActiveController
     }
 
 
-    public function actionIndex(): string
+    /**
+     * @throws MethodNotAllowedHttpException
+     * @throws ServerErrorHttpException
+     */
+    public function actionIndex(): array
     {
-        return $this->render('index');
+        $request = Yii::$app->request;
+
+        if ($request->isGet) {
+            return $this->getAllPosts($request);
+        } else {
+            throw new MethodNotAllowedHttpException;
+        }
     }
 
+    /**
+     * @throws MethodNotAllowedHttpException
+     * @throws ServerErrorHttpException
+     */
+    public function actionMyPosts(): array
+    {
+        $request = Yii::$app->request;
 
+        if ($request->isGet) {
+            return $this->getMyPosts($request);
+        } else {
+            throw new MethodNotAllowedHttpException;
+        }
+    }
 
+    /**
+     * @throws ServerErrorHttpException
+     */
+    private function getMyPosts($request): array
+    {
+        $accessToken = $request->get('accessToken');
+
+        if ($accessToken == null) {
+            throw new ServerErrorHttpException('AccessToken should not be empty');
+        }
+
+        return [];
+    }
+
+    /**
+     * @throws ServerErrorHttpException
+     */
+    private function getAllPosts($request): array
+    {
+        $accessToken = $request->get('accessToken');
+
+        if ($accessToken == null) {
+            throw new ServerErrorHttpException('AccessToken should not be empty');
+        }
+
+        return [];
+    }
 }
