db23114a59f9644819a55a16b1dac7cdadfcdad5
Стас Гайсин <stas.gaisinn@gmail.com>
2022-10-02 23:07:09 +0300

    Cleaned code

--- frontend/controllers/LoginController.php
+++ frontend/controllers/LoginController.php
@@ -6,10 +6,12 @@ use DateTimeImmutable;
 use Yii;
 use yii\base\Exception;
 use yii\web\Controller;
+use yii\web\MethodNotAllowedHttpException;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
 use common\models\User;
 use common\models\AccessToken;
+use yii\web\ServerErrorHttpException;
 
 class LoginController extends Controller
 {
@@ -30,6 +32,7 @@ class LoginController extends Controller
 
     /**
      * @throws NotFoundHttpException
+     * @throws Exception
      */
     public function actionIndex()
     {
@@ -38,38 +41,46 @@ class LoginController extends Controller
         if ($request->isPost) {
             return $this->login($request);
         } else {
-            throw new \yii\web\MethodNotAllowedHttpException;
+            throw new MethodNotAllowedHttpException;
         }
     }
-
-
+    
     /**
      * @throws Exception
      */
-    private function login($request) : array {
+    private function login($request): array
+    {
         $username = $request->post('username');
         $password = $request->post('password');
 
         if ($password == null || $username == null) {
-            throw new \yii\web\ServerErrorHttpException('Username and password should not be empty');
+            throw new ServerErrorHttpException('Username and password should not be empty');
         }
 
         $user = User::findOne(['username' => $username]);
 
-        if ($user != null) {
-            $accessToken = new AccessToken();
-            $date = new DateTimeImmutable();
-
-            $accessToken->userId = $user->userId;
-            $accessToken->token = Yii::$app->security->generateRandomString() . '_' . time();
-            $accessToken->createdAt = $date->getTimestamp();
-
-            $accessToken->save();
-
-            return ['accessToken' => $accessToken->token];
-        } else {
-            throw new \yii\web\NotFoundHttpException('User not found');
+        if ($user == null || !$user->validatePassword($password)) {
+            throw new NotFoundHttpException('User not found');
         }
+
+        $accessToken = $this->createAccessToken($user->userId);
+        $accessToken->save();
+
+        return ['accessToken' => $accessToken->token];
     }
 
+    /**
+     * @throws Exception
+     */
+    private static function createAccessToken(int $userId): AccessToken
+    {
+        $accessToken = new AccessToken();
+        $date = new DateTimeImmutable();
+
+        $accessToken->userId = $userId;
+        $accessToken->token = Yii::$app->security->generateRandomString() . '_' . time();
+        $accessToken->createdAt = $date->getTimestamp();
+
+        return $accessToken;
+    }
 }
