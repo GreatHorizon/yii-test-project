1d4d56409d87020aa63641d4d11024d60b7e40fa
Стас Гайсин <stas.gaisinn@gmail.com>
2022-10-02 23:27:09 +0300

    Removed unused commented code

--- console/migrations/m221002_154112_change_created_at_time_type.php
+++ console/migrations/m221002_154112_change_created_at_time_type.php
@@ -21,21 +21,5 @@ class m221002_154112_change_created_at_time_type extends Migration
     public function safeDown()
     {
         $this->alterColumn('access_token', 'createdAt', $this->dateTime());
-
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m221002_154112_change_created_at_time_type cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
